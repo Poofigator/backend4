a8ff9ef13ee45b4e3cc95f2d658857afce0e0471
Poofigator <77187509+Poofigator@users.noreply.github.com>
2024-11-10 17:16:11 +0400

    change port

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ if __name__ == '__main__':
     conn.commit()
     cursor.close()
     conn.close()
-    app.run(debug=True, port=80) # Запуск сервера на порту 80 с отладкой
+    app.run(debug=True, host='0.0.0.0', port=81) # Запуск сервера на порту 81 с отладкой
